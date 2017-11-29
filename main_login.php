2c761b352d8fb1530851a95bb2a62ad3461456e7
艾吉104 <68104@ms23.url.com.tw>
2017-11-29 19:54:34 +0800

    登入頁面恢復
    製作過程中登入表單不小心變成投稿表單

--- main_login.php
+++ main_login.php
@@ -2,8 +2,8 @@
 require_once '_header.php';
 $page_title = '學員登入';
 $op         = '';
-// if (isset($_SESSION['username'])) {
-//     header("location:index.php");
-// }
+if (isset($_SESSION['username'])) {
+    header("location:index.php");
+}
 
 require_once '_footer.php';
